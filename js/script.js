03e3c233c79ff0b1159f83236b5580ce07cd70ac
Kstrohfus <houston11@me.com>
2022-11-04 23:55:16 -0700

    Added user info to page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-alert(hello)
+//main div overview of everything
+const mainOverview = document.querySelector(".overview")
+const username = "kstrohfus"
+
+const ghUser = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`)
+    const data = await userInfo.json();
+
+gitHubInfo(data);
+
+};
+
+ghUser();
+
+const gitHubInfo = function (data) {
+    const divInfo = document.createElement("div");
+    
+    divInfo.classList.add("user-info")
+    divInfo.innerHTML = `<figure>
+      <img alt="user avatar" src=${"https://avatars.githubusercontent.com/u/111642391?v=4"} />
+    </figure>
+    <div>
+      <p><strong>Name:</strong> ${"Kayla Strohfus"}</p>
+      <p><strong>Bio:</strong> ${"Front End Web Development Student / Office Manager"}</p>
+      <p><strong>Location:</strong> ${"Phoenix, AZ"}</p>
+      <p><strong>Number of public repos:</strong> ${37}</p>
+    </div>`
+
+    mainOverview.append(divInfo);
+}
